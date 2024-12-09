71fbf99444c0441f7510df609389b44077b8ff4b
NZhengye <thamasnanx@gmail.com>
2024-12-09 14:51:41 +0800

    111

--- src/view/game/GameFrame.java
+++ src/view/game/GameFrame.java
@@ -14,11 +14,11 @@ public class GameFrame extends JFrame {
     private JButton restartBtn;
     private JButton loadBtn;
     private JButton returnBtn;
-    private JButton LeftBtn;
-    private JButton RightBtn;
-    private JButton UpBtn;
-    private JButton DownBtn;
-    private JButton SaveBtn;
+    private JButton leftBtn;
+    private JButton rightBtn;
+    private JButton upBtn;
+    private JButton downBtn;
+    private JButton saveBtn;
 
     private JLabel stepLabel;
     private GamePanel gamePanel;
@@ -38,11 +38,11 @@ public class GameFrame extends JFrame {
         this.restartBtn = FrameUtil.createButton(this, "Restart", new Point(gamePanel.getWidth() + 80, 120), 80, 40);
         this.loadBtn = FrameUtil.createButton(this, "Load", new Point(gamePanel.getWidth() + 80, 260), 80, 40);
         this.returnBtn = FrameUtil.createButton(this, "Return", new Point(gamePanel.getWidth() + 80, 190), 80, 40);
-        this.LeftBtn = FrameUtil.createButton(this,"⬅",new Point(gamePanel.getWidth() + 190,210),20,20);
-        this.RightBtn = FrameUtil.createButton(this,"➡",new Point(gamePanel.getWidth() + 250,210),20,20);
-        this.UpBtn = FrameUtil.createButton(this,"⬆",new Point(gamePanel.getWidth() + 220,180),20,20);
-        this.RightBtn = FrameUtil.createButton(this,"⬇",new Point(gamePanel.getWidth() + 220,240),20,20);
-        this.SaveBtn = FrameUtil.createButton(this,"Save",new Point(gamePanel.getWidth() + 80,330),80,40);
+        this.leftBtn = FrameUtil.createButton(this,"⬅",new Point(gamePanel.getWidth() + 190,210),20,20);
+        this.rightBtn = FrameUtil.createButton(this,"➡",new Point(gamePanel.getWidth() + 250,210),20,20);
+        this.upBtn = FrameUtil.createButton(this,"⬆",new Point(gamePanel.getWidth() + 220,180),20,20);
+        this.downBtn = FrameUtil.createButton(this,"⬇",new Point(gamePanel.getWidth() + 220,240),20,20);
+        this.saveBtn = FrameUtil.createButton(this,"Save",new Point(gamePanel.getWidth() + 80,330),80,40);
 
         /*创建了一个 JLabel 来显示游戏的当前步骤或状态。这个标签也相对于 GamePanel 进行定位，并添加到窗口中。
          *GamePanel 被设置为根据需要更新此标签。*/
@@ -66,7 +66,23 @@ public class GameFrame extends JFrame {
             LevelFrame.getFrameController().returnLevelFrame(this);
             gamePanel.requestFocusInWindow();//enable key listener
         });
-        //todo: add other button here such as load, save, back, (up, down, left, right) through mouseclick...
+        this.upBtn.addActionListener(e -> {
+            gamePanel.doMoveUp();
+            gamePanel.requestFocusInWindow();//enable key listener
+        });
+        this.downBtn.addActionListener(e -> {
+            gamePanel.doMoveDown();
+            gamePanel.requestFocusInWindow();//enable key listener
+        });
+        this.leftBtn.addActionListener(e -> {
+            gamePanel.doMoveLeft();
+            gamePanel.requestFocusInWindow();//enable key listener
+        });
+        this.rightBtn.addActionListener(e -> {
+            gamePanel.doMoveRight();
+            gamePanel.requestFocusInWindow();//enable key listener
+        });
+        //todo: add other button here such as , back, through mouseclick...
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
